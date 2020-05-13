a8904e12c3a39705c06e455b2ca1eb21f00b219b
toshokan <toshokan@shojigate.net>
2020-05-12 21:47:56 -0400

    Start routing and error handling

--- tomiko-auth/src/bin/tomikod.rs
+++ tomiko-auth/src/bin/tomikod.rs
@@ -1,11 +1,15 @@
 use warp::Filter;
 use serde::{Deserialize, Deserializer};
 
+use std::convert::Infallible;
+
+use sqlx::sqlite::SqlitePool;
+
 #[derive(Debug, Deserialize)]
 struct AuthRequest {
     response_type: ResponseType,
     client_id: ClientId,
-    redirect_uri: String, // TODO
+    redirect_uri: RedirectUri,
     scope: Scope,
     state: String
 }
@@ -15,7 +19,7 @@ struct TokenRequest {
     grant_type: GrantType,
     client_id: ClientId,
     client_secret: ClientSecret,
-    redirect_uri: String, // TODO,
+    redirect_uri: RedirectUri,
     code: AuthCode
 }
 
@@ -34,17 +38,52 @@ enum ResponseType {
     Code
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(sqlx::Type)]
+#[sqlx(transparent)]
+#[derive(serde::Deserialize)]
 #[serde(transparent)]
 struct ClientId(String);
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug)]
+#[derive(sqlx::Type)]
+#[sqlx(transparent)]
+#[derive(serde::Deserialize)]
+#[serde(transparent)]
+struct RedirectUri(String);
+
+#[derive(Debug)]
+#[derive(sqlx::Type)]
+#[sqlx(transparent)]
+#[derive(serde::Deserialize)]
 #[serde(transparent)]
 struct ClientSecret(String);
 
 #[derive(Debug, Deserialize)]
 #[serde(transparent)]
 struct AuthCode(String);
+
+#[derive(Debug)]
+#[derive(sqlx::FromRow)]
+struct Client {
+    id: ClientId,
+    secret: ClientSecret
+}
+
+#[derive(Debug)]
+#[derive(sqlx::FromRow)]
+struct RedirectRecord {
+    #[sqlx(rename="client_id")]
+    id: ClientId,
+    uri: RedirectUri
+}
+
+#[derive(Debug)]
+enum Error {
+    UnregisteredUri(RedirectUri)
+}
+
+impl warp::reject::Reject for Error {}
 
 impl<'de> Deserialize<'de> for Scope {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -57,17 +96,63 @@ impl<'de> Deserialize<'de> for Scope {
     }
 }
 
+async fn ensure_uri(db: &SqlitePool, req: &AuthRequest) -> Result<(), warp::Rejection> {
+    use sqlx::sqlite::SqliteQueryAs;
+    
+    let mut conn = db.acquire().await.unwrap();
+    let result = sqlx::query_as::<_, RedirectRecord>("SELECT * FROM uris WHERE client_id = ? AND uri = ?")
+        .bind(&req.client_id)
+        .bind(&req.redirect_uri)
+	.fetch_optional(&mut conn).await;
+
+    if let Ok(r) = result {
+	if r.is_some() {
+	    return Ok(())
+	}
+    }
+    
+    Err(warp::reject::custom(Error::UnregisteredUri(req.redirect_uri.clone())))
+}
+
+async fn authorize(db: SqlitePool, req: AuthRequest) -> Result<String, warp::Rejection> {
+    ensure_uri(&db, &req).await?;
+    
+    Ok(debug_req(req))
+}
+
 fn debug_req<R: std::fmt::Debug>(r: R) -> String {
     format!("Request {:?}", r)
 }
 
+fn random_string(size: usize) -> String {
+    use rand::Rng;
+    
+    rand::thread_rng()
+        .sample_iter(rand::distributions::Alphanumeric)
+        .take(size)
+        .collect()
+}
+
+fn with_db(pool: SqlitePool) -> impl Filter<Extract = (SqlitePool,), Error = Infallible> + Clone {
+    warp::any().map(move || pool.clone())
+}
+
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL").expect("set DATABASE_URL");
+    
+    let pool = SqlitePool::builder()
+        .max_size(1)
+        .build(&db_url).await.unwrap();
+    
     let oauth = warp::path("oauth");
 
     let auth = warp::path("auth")
+        .and(with_db(pool.clone()))
 	.and(warp::filters::query::query())
-        .map(debug_req::<AuthRequest>);
+        .and_then(authorize);
 
     let token = warp::path("token")
         .and(warp::filters::method::post())
