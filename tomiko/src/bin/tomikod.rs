635d686799c251c88c4b8ac499fe82d47f9c3fef
toshokan <toshokan@shojigate.net>
2020-05-13 23:45:30 -0400

    start driver

--- tomiko/src/bin/tomikod.rs
+++ tomiko/src/bin/tomikod.rs
@@ -1,22 +1,27 @@
+use tomiko_auth::{AuthenticationCodeFlow,
+		  AuthorizationRequest, AuthorizationResponse, AuthorizationError,
+		  TokenRequest, AccessTokenResponse, AccessTokenError};
+
 use tomiko_http::server::Server;
 
 use async_trait::async_trait;
 
-struct Dummy;
+#[derive(Default)]
+struct OAuthDriver;
 
 #[async_trait]
-impl tomiko_auth::AuthenticationCodeFlow for Dummy {
-    async fn authorization_request(req: tomiko_auth::AuthorizationRequest) -> Result<tomiko_auth::AuthorizationResponse, tomiko_auth::AuthorizationError> {
-	panic!("Yo")
+impl AuthenticationCodeFlow for OAuthDriver {
+    async fn authorization_request(req: AuthorizationRequest) -> Result<AuthorizationResponse, AuthorizationError> {
+	panic!("auth_req")
     }
 
-    async fn access_token_request<T>(req: tomiko_auth::TokenRequest) -> Result<tomiko_auth::AccessTokenResponse<T>, tomiko_auth::AccessTokenError> {
-	panic!("Yo2")
+    async fn access_token_request<T>(req: TokenRequest) -> Result<AccessTokenResponse<T>, AccessTokenError> {
+	panic!("access_token_req")
     }
 }
 
 async fn tomikod() -> Option<()> {
-    let dummy = Dummy;
+    let dummy = OAuthDriver;
     let server = Server::new(dummy);
     server.serve().await;
     Some(())
