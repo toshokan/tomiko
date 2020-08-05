787ec74f2283720733d2047e0121963368261c66
toshokan <toshokan@shojigate.net>
2020-08-05 00:18:34 -0400

    Put HashingService in tomiko-util

---
+++ tomiko-util/src/hash.rs
@@ -0,0 +1,48 @@
+use tomiko_core::types::{ClientId, ClientSecret};
+
+#[derive(Debug)]
+pub struct HashedClientSecret(pub String);
+
+impl HashedClientSecret {
+    pub fn from_raw(raw: String) -> Self {
+        Self(raw)
+    }
+}
+
+#[derive(Debug)]
+pub struct HashedClientCredentials {
+    pub client_id: ClientId,
+    pub client_secret: HashedClientSecret,
+}
+
+#[derive(Debug)]
+pub struct HashingService {
+    secret_key: String,
+}
+
+impl HashingService {
+    pub fn with_secret_key(secret_key: String) -> Self {
+        Self { secret_key }
+    }
+
+    pub fn hash(&self, secret: &ClientSecret) -> Result<HashedClientSecret, ()> {
+        let mut hasher = argonautica::Hasher::default();
+        let hash = hasher
+            .with_password(&secret.0)
+            .with_secret_key(&self.secret_key)
+            .hash()
+            .expect("Failed to hash"); // TODO
+
+        Ok(HashedClientSecret(hash))
+    }
+
+    pub fn verify(&self, secret: &ClientSecret, hashed: &HashedClientSecret) -> Result<bool, ()> {
+        let mut verifier = argonautica::Verifier::default();
+        verifier
+            .with_secret_key(&self.secret_key)
+            .with_password(&secret.0)
+            .with_hash(&hashed.0)
+            .verify()
+            .map_err(|_| ())
+    }
+}
