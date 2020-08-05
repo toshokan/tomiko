5214265f57713f0788850d171970eeb48feb7249
toshokan <toshokan@shojigate.net>
2020-08-05 17:45:00 -0400

    Add setup migration

---
+++ migrations/setup.sql
@@ -0,0 +1,3 @@
+CREATE TABLE uris(client_id TEXT NOT NULL, uri TEXT NOT NULL);
+CREATE TABLE clients(client_id TEXT NOT NULL, secret_hash TEXT NOT NULL);
+CREATE TABLE codes(client_id TEXT NOT NULL, code TEXT NOT NULL, state TEXT, uri TEXT NOT NULL, scope TEXT);
