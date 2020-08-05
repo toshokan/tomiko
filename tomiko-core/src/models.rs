d94a57595d3875eae549b036dd9f4be51cd8052c
toshokan <toshokan@shojigate.net>
2020-08-05 09:38:00 -0400

    Move models into their own module

---
+++ tomiko-core/src/models.rs
@@ -0,0 +1,23 @@
+use crate::types::*;
+
+#[derive(Debug)]
+pub struct Client {
+    pub id: ClientId,
+    pub secret: HashedClientSecret,
+}
+
+#[derive(Debug)]
+pub struct AuthCodeData {
+    pub code: AuthCode,
+    pub client_id: ClientId,
+    pub state: Option<String>,
+    pub redirect_uri: RedirectUri,
+    pub scope: Option<Scope>,
+}
+
+#[derive(Debug)]
+pub struct RedirectRecord {
+    pub client_id: ClientId,
+    pub uri: RedirectUri,
+
+}
